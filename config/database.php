ce41866c49f550ee9c150d853836ed3ad5baed20
Dada <daviddada360@gmail.com>
2024-12-24 01:44:09 +0100

    added stuff

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
