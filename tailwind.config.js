730664a0063cb6a3d2fc57170bacd407c5352526
Dada <daviddada360@gmail.com>
2025-01-13 10:24:28 +0100

    Fixing vite issues

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    'resources\views\auth\reset-password.blade.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
